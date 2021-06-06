1eaec503897049d15b292eb7f151c12f5180b961
Lew Milburn <lewiscmilburn@gmail.com>
2021-06-07 00:50:08 +0100

    Updated to const syntax

--- theme.php
+++ theme.php
@@ -1,9 +1,9 @@
 <?php
     /* Website Theme */
-    define('THEME_NAME', 'Andromeda');
+    const THEME_NAME = 'Andromeda';
     /* Panel Theme */
-    define('THEME_PANEL_FONT', 'Questrial'); /* Google Font Name */
-    define('THEME_PANEL_COLOUR', 'gray');
+    const THEME_PANEL_FONT = 'Questrial'; /* Google Font Name */
+    const THEME_PANEL_COLOUR = 'gray';
     /* Site Branding */
-    define('THEME_PANEL_BACKGROUND_IMAGE', CONFIG_INSTALL_URL.'/assets/panel/images/background.jpg');
-    define('THEME_SOCIAL_IMAGE', CONFIG_INSTALL_URL.'/assets/images/social-image.jpg');
+    const THEME_PANEL_BACKGROUND_IMAGE = CONFIG_INSTALL_URL.'/assets/panel/images/background.jpg';
+    const THEME_SOCIAL_IMAGE = CONFIG_INSTALL_URL.'/assets/images/social-image.jpg';
