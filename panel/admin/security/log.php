5f0558a3724d71a7cb8be79d648dd34effe346e3
Lew Milburn <lewiscmilburn@gmail.com>
2021-06-07 15:48:03 +0100

    Added title attribute

--- panel/admin/security/log.php
+++ panel/admin/security/log.php
@@ -19,7 +19,7 @@
 
             <h1 class="text-gray-900 text-3xl">Security Log</h1>
             <br>
-            <iframe src="<?php echo CONFIG_INSTALL_URL; ?>/assets/storage/security_log.txt" width="100%" height="75%">
+            <iframe src="<?php echo CONFIG_INSTALL_URL; ?>/assets/storage/security_log.txt" width="100%" height="75%" title="Security Log">
         </div>
     </body>
 </html>
