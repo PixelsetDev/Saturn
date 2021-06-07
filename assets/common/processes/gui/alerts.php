4383ce3b458f29f777fda4def669c5259c5832b5
Lew Milburn <lewiscmilburn@gmail.com>
2021-06-07 18:07:48 +0100

    Refactored code

--- assets/common/processes/gui/alerts.php
+++ assets/common/processes/gui/alerts.php
@@ -2,32 +2,32 @@
 function alert($type,$message): string {
     $message = checkOutput('DEFAULT', $message);
     if($type == 'ERROR') {
-        return '<br>
+        echo '<br>
                             <div class="duration-300 transform bg-red-100 border-l-4 border-red-500 hover:-translate-y-2">
                                 <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
                                     <h6 class="mb-2 font-semibold leading-5">[ERROR] '.$message.'</h6>
                                 </div>
                             </div>';
     } else if ($type == 'WARNING') {
-        return '<br><div class="duration-300 transform bg-yellow-100 border-l-4 border-yellow-500 hover:-translate-y-2">
+        echo '<br><div class="duration-300 transform bg-yellow-100 border-l-4 border-yellow-500 hover:-translate-y-2">
                                 <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
                                     <h6 class="mb-2 font-semibold leading-5">[WARNING] '.$message.'</h6>
                                 </div>
                             </div>';
     } else if ($type == 'SUCCESS') {
-        return '<br><div class="duration-300 transform bg-green-100 border-l-4 border-green-500 hover:-translate-y-2">
+        echo '<br><div class="duration-300 transform bg-green-100 border-l-4 border-green-500 hover:-translate-y-2">
                                 <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
                                     <h6 class="mb-2 font-semibold leading-5">'.$message.'</h6>
                                 </div>
                             </div>';
     } else if ($type == 'INFO') {
-        return '<br><div class="duration-300 transform bg-blue-100 border-l-4 border-blue-500 hover:-translate-y-2">
+        echo '<br><div class="duration-300 transform bg-blue-100 border-l-4 border-blue-500 hover:-translate-y-2">
                                 <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
                                     <h6 class="mb-2 font-semibold leading-5">'.$message.'</h6>
                                 </div>
                             </div>';
     } else {
-        return '<br><div class="duration-300 transform bg-gray-100 border-l-4 border-gray-500 hover:-translate-y-2">
+        echo '<br><div class="duration-300 transform bg-gray-100 border-l-4 border-gray-500 hover:-translate-y-2">
                                 <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
                                     <h6 class="mb-2 font-semibold leading-5">'.$message.'</h6>
                                 </div>
--- panel/pages/editor/index.php
+++ panel/pages/editor/index.php
@@ -69,9 +69,9 @@
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>/?pageID=<?php echo $pageID; ?>" method="POST" class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <?php
                 if(isset($_GET['error'])) {
-                    echo alert('ERROR', $_GET['error']);
+                    alert('ERROR', $_GET['error']);
                 } else if(isset($_GET['success'])) {
-                    echo alert('SUCCESS', $_GET['success']);
+                    alert('SUCCESS', $_GET['success']);
                 }
             ?>
 
