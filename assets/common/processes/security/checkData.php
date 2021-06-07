8234d54d802650cf4669228a85198f61275962fb
Lew Milburn <lewiscmilburn@gmail.com>
2021-06-07 01:03:06 +0100

    Typo

--- assets/common/processes/security/checkData.php
+++ assets/common/processes/security/checkData.php
@@ -1,8 +1,10 @@
 <?php
-    function checkData( $data, $alert, $illegalCode, $replacement) {
+    function checkData( $data, $alert, $illegalCode, $replacement): string {
         if (strpos($data, $illegalCode) !== false) { log_security_blocked($alert);
             if(SECURITY_MODE == 'clean') {
                 $data = str_ireplace($data, $illegalCode, $replacement);
             } else if (SECURITY_MODE == 'stop') {header('Location: '.CONFIG_INSTALL_URL.'/panel/system/error/?err=security');exit;}
         }
+
+        return $data;
     }
