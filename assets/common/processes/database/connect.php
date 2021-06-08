e5cc1502722f93a28eff5c18eb05ba7880cfeb87
Lew Milburn <lewiscmilburn@gmail.com>
2021-06-08 12:01:17 +0100

    Removed commented-out code. Removed literal 'true' boolean values.

--- assets/common/processes/database/connect.php
+++ assets/common/processes/database/connect.php
@@ -1,13 +1,14 @@
 <?php
     $conn = mysqli_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME, DATABASE_PORT);
     if (mysqli_connect_errno($conn)) {
-        if(CONFIG_DEBUG == true) {
+        if(CONFIG_DEBUG === true) {
             errorHandlerError('x', 'Failed to connect to MySQL Database. Please check your config.php file and try again. Error: '.mysqli_connect_error());
+            log_console('Saturn][Database', 'Unable to connect to the Database.');
         }
         errorHandlerRedirect('db_conn');
     } else {
-        if(CONFIG_DEBUG == true) {
-            /*log_console('Saturn][Database', 'Connected to the Database.');*/
+        if(CONFIG_DEBUG === true) {
+            log_console('Saturn][Database', 'Connected to the Database.');
         }
     }
     $query = "SET time_zone = '".CONFIG_SITE_TIMEZONE."';";
