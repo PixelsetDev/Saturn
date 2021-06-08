f992137924d31638c34fb9c281784352dbb3332b
Lew Milburn <lewiscmilburn@gmail.com>
2021-06-09 00:58:51 +0100

    Fixed caching issue.

--- panel/admin/security/log.php
+++ panel/admin/security/log.php
@@ -35,7 +35,11 @@
                 }
             ?>
             <br>
-            <iframe src="<?php echo CONFIG_INSTALL_URL; ?>/assets/storage/security_log.txt" class="w-full h-3/4 overflow-scroll" title="Security Log"></iframe>
+            <iframe src="<?php echo CONFIG_INSTALL_URL; ?>/assets/storage/security_log.txt?cache=<?php try {
+                echo random_int('0000', '9999');
+            } catch (Exception $e) {
+                echo alert('ERROR',$e);
+            } ?>" class="w-full h-3/4 overflow-scroll" title="Security Log"></iframe>
             <div x-data="{open:false}">
                 <a @click="open = true" class="flex-grow transition-all duration-200 hover:shadow-lg cursor-pointer flex w-1/6 items-center justify-center px-8 py-1 border border-transparent text-base font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-200 md:py-1 md:text-rg md:px-10">Clear Log</a>
                 <?php echo display_modal('red', 'Clear Log', 'Are you sure you want to clear the Security Log?<br>This action cannot be undone.<br><br>If you clear the log you loose all existing logged data. Your username will be recorded as the person who cleared this log at the top of the new log.', '<form action="'.htmlspecialchars($_SERVER['PHP_SELF']).'" method="post"><div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse flex">
