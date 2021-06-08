1e2a3ed9da06cc2215f2a9f7ac7dd1bbd1576206
Lewis Milburn <contact@lewismilburn.com>
2021-06-08 15:14:04 +0100

    Removed lines causing errors when CONFIG_DEBUG set to true.

--- assets/common/processes/database/connect.php
+++ assets/common/processes/database/connect.php
@@ -3,14 +3,10 @@
     if (mysqli_connect_errno($conn)) {
         if(CONFIG_DEBUG === true) {
             errorHandlerError('x', 'Failed to connect to MySQL Database. Please check your config.php file and try again. Error: '.mysqli_connect_error());
-            log_console('Saturn][Database', 'Unable to connect to the Database.');
         }
         errorHandlerRedirect('db_conn');
-    } else {
-        if(CONFIG_DEBUG === true) {
-            log_console('Saturn][Database', 'Connected to the Database.');
-        }
     }
+
     $query = "SET time_zone = '".CONFIG_SITE_TIMEZONE."';";
     $rs = mysqli_query($conn,$query);
     unset($query,$rs);
