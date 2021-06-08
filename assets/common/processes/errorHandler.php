395d33f78ffe123e26bc20ab6928a58230102755
Lewis Milburn <contact@lewismilburn.com>
2021-06-08 15:05:50 +0100

    Refactored code.

--- assets/common/processes/errorHandler.php
+++ assets/common/processes/errorHandler.php
@@ -1,6 +1,6 @@
 <?php
     function errorHandlerWarning($errno, $errstr) {
-        if(CONFIG_PHP_ERRORS == true) {
+        if(CONFIG_DEBUG == true) {
             echo '<div class="duration-300 transform bg-yellow-100 border-l-4 border-yellow-500">
                         <div class="h-auto p-5 border border-l-0 rounded-r shadow-sm">
                             <h6 class="mb-2 font-semibold leading-5">['.date("H:i").'][WARNING]['.$errno.'] <span class="font-medium">' . $errstr . '</span></h6>
@@ -9,7 +9,7 @@
         }
     }
     function errorHandlerError($errno, $errstr) {
-        if(CONFIG_PHP_ERRORS == true) {
+        if(CONFIG_DEBUG == true) {
             echo '<div class="duration-300 transform bg-red-100 border-l-4 border-red-500">
                                 <div class="h-auto p-5 border border-l-0 rounded-r shadow-sm">
                                     <h6 class="mb-2 font-semibold leading-5">['.date("H:i").'][ERROR]['.$errno.'] <span class="font-medium">' . $errstr . '</span></h6>
@@ -18,10 +18,10 @@
         }
     }
     function errorHandlerNotice($errno, $errstr) {
-        if(CONFIG_PHP_NOTICES == true) {
+        if(CONFIG_DEBUG == true) {
             echo '<div class="duration-300 transform bg-blue-100 border-l-4 border-blue-500">
                                 <div class="h-auto p-5 border border-l-0 rounded-r shadow-sm">
-                                    <h6 class="mb-2 font-semibold leading-5">['.date("H:i").'][NOTICE] <span class="font-medium">' . $errstr . '</span></h6>
+                                    <h6 class="mb-2 font-semibold leading-5">['.date("H:i").'][NOTICE]['.$errno.'] <span class="font-medium">' . $errstr . '</span></h6>
                                 </div>
                             </div>';
         }
