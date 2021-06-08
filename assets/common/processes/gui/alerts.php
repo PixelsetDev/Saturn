20a6f422e2e3f1b5c1b394e335d884d5522b6221
lewmilburn <lewismilburn@icloud.com>
2021-06-08 17:45:42 +0100

    Added 'static_alert' function.

--- assets/common/processes/gui/alerts.php
+++ assets/common/processes/gui/alerts.php
@@ -35,3 +35,39 @@ function alert($type,$message) {
     }
     return true;
 }
+
+function static_alert($type,$message) {
+    $message = checkOutput('DEFAULT', $message);
+    if($type == 'ERROR') {
+        echo '<div class="bg-red-100 border-l-4 border-red-500 hover:-translate-y-2">
+                                <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                                    <h6 class="mb-2 font-semibold leading-5">[ERROR] '.$message.'</h6>
+                                </div>
+                            </div>';
+    } else if ($type == 'WARNING') {
+        echo '<div class="bg-yellow-100 border-l-4 border-yellow-500 hover:-translate-y-2">
+                                <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                                    <h6 class="mb-2 font-semibold leading-5">[WARNING] '.$message.'</h6>
+                                </div>
+                            </div>';
+    } else if ($type == 'SUCCESS') {
+        echo '<div class="bg-green-100 border-l-4 border-green-500 hover:-translate-y-2">
+                                <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                                    <h6 class="mb-2 font-semibold leading-5">'.$message.'</h6>
+                                </div>
+                            </div>';
+    } else if ($type == 'INFO') {
+        echo '<div class="bg-blue-100 border-l-4 border-blue-500 hover:-translate-y-2">
+                                <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                                    <h6 class="mb-2 font-semibold leading-5">'.$message.'</h6>
+                                </div>
+                            </div>';
+    } else {
+        echo '<div class="bg-gray-100 border-l-4 border-gray-500 hover:-translate-y-2">
+                                <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                                    <h6 class="mb-2 font-semibold leading-5">'.$message.'</h6>
+                                </div>
+                            </div>';
+    }
+    return true;
+}
