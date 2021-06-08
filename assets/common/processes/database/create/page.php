e910ccbe49105bc2aebf93389a85ed6484648f38
Lew Milburn <lewiscmilburn@gmail.com>
2021-06-08 12:05:56 +0100

    Refactored code.

--- assets/common/processes/database/create/page.php
+++ assets/common/processes/database/create/page.php
@@ -3,10 +3,11 @@
         global $conn;
 
         $query = "INSERT INTO `".DATABASE_PREFIX."pages` (`id`, `user_id`, `category_id`, `url`, `template`, `title`, `description`, `content`, `reference`) VALUES (NULL, '".$_SESSION['id']."', '".$categoryID."', '".$url."', '".$template."', '".$title."', '".$description."', NULL, NULL)";
+        $query2 = "INSERT INTO `".DATABASE_PREFIX."pages_pending` (`id`, `user_id`, `category_id`, `title`, `content`, `reference`) VALUES (NULL, '0', '".$categoryID."', NULL, NULL, NULL)";
 
-        if(mysqli_query($conn, $query)){}else{return false;}
-
-        $query = "INSERT INTO `".DATABASE_PREFIX."pages_pending` (`id`, `user_id`, `category_id`, `title`, `content`, `reference`) VALUES (NULL, '0', '".$categoryID."', NULL, NULL, NULL)";
-
-        if(mysqli_query($conn, $query)){return true;}else{return false;}
+        if(mysqli_query($conn, $query) && mysqli_query($conn, $query2)) {
+            return true;
+        } else {
+            return false;
+        }
     }
