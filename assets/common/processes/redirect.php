3260f466cab557b16ed60c59ecc14af2dddac484
Lew Milburn <lewiscmilburn@gmail.com>
2021-06-08 23:15:49 +0100

    Added redirect fallback.

--- assets/common/processes/redirect.php
+++ assets/common/processes/redirect.php
@@ -1,10 +1,12 @@
 <?php
     function internal_redirect($url) {
         header('Location: ' . CONFIG_INSTALL_URL . $url);
+        echo '<meta http-equiv="refresh" content="0; url='.CONFIG_INSTALL_URL.$url.'">';
         exit;
     }
 
     function redirect($url) {
         header('Location: ' . $url);
+        echo '<meta http-equiv="refresh" content="0; url='.$url.'">';
         exit;
     }
