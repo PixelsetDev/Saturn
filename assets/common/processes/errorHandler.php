c7aae23ac12d8865c3e14a746eddbe0e1f810949
Lew Milburn <lewiscmilburn@gmail.com>
2021-06-09 00:24:55 +0100

    Fixed missing check.

--- assets/common/processes/errorHandler.php
+++ assets/common/processes/errorHandler.php
@@ -1,22 +1,24 @@
 <?php
     function errorHandlerWarning($errno, $errstr) {
-        errorHandler($errno, $errstr, 'WARNING', 'yellow');
+        if(CONFIG_DEBUG) {
+            errorHandler($errno, $errstr, 'WARNING', 'yellow');
+        }
     }
 
     function errorHandlerError($errno, $errstr) {
-        if(CONFIG_DEBUG === true) {
+        if(CONFIG_DEBUG) {
             errorHandler($errno, $errstr, 'ERROR', 'red');
         }
     }
 
     function errorHandlerNotice($errno, $errstr) {
-        if(CONFIG_DEBUG === true) {
+        if(CONFIG_DEBUG) {
             errorHandler($errno, $errstr, 'NOTICE', 'blue');
         }
     }
 
     function errorHandler($errno, $errstr, $type, $colour) {
-        if(CONFIG_DEBUG === true) {
+        if(CONFIG_DEBUG) {
             echo '<div class="duration-300 transform bg-'.$colour.'-100 border-l-4 border-'.$colour.'-500">
                                 <div class="h-auto p-5 border border-l-0 rounded-r shadow-sm">
                                     <h6 class="mb-2 font-semibold leading-5">['.date("H:i").']['.$type.']['.$errno.'] <span class="font-medium">'.$errstr.'</span></h6>
