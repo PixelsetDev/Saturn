b1498cb2c038b62e39679337b163a9e1246f39ab
Lew Milburn <lewiscmilburn@gmail.com>
2021-06-10 21:05:26 +0100

    Added data checking.

--- assets/common/processes/gui/modals.php
+++ assets/common/processes/gui/modals.php
@@ -1,6 +1,10 @@
 <?php
 
     function display_modal_sidebar($title, $contents, $footer): string {
+        $title = checkInput('DEFAULT', $title);
+        $contents = checkInput('HTML', $contents);
+        $footer = checkInput('HTML', $footer);
+
         return '<div class="fixed inset-0 overflow-hidden z-50" x-show="open" @click.away="open = false">
                                         <div class="absolute inset-0 overflow-hidden">
                                             <div class="absolute inset-0 bg-gray-500 bg-opacity-75" @click="open=false"></div>
@@ -34,7 +38,31 @@
                                     </div>';
     }
 
-    function display_modal($colour, $title, $contents, $footer): string {
+    function display_modal($type, $title, $contents, $footer): string {
+        $type = checkInput('DEFAULT', $type);
+        $title = checkInput('DEFAULT', $title);
+        $contents = checkInput('HTML', $contents);
+        $footer = checkInput('HTML', $footer);
+
+        if($type == 'ERROR' || $type == 'red') {
+            $colour = 'red';
+            $icon = 'fas fa-times';
+        } else if ($type == 'WARNING' || $type == 'yellow') {
+            $colour = 'yellow';
+            $icon = 'fas fa-exclamation';
+        } else if ($type == 'SUCCESS' || $type == 'green') {
+            $colour = 'green';
+            $icon = 'fas fa-check';
+        } else if ($type == 'INFO' || $type == 'blue') {
+            $colour = 'blue';
+            $icon = 'fas fa-info';
+        } else if ($type == 'NOTIFICATION' || $type == 'gray') {
+            $colour = 'gray';
+            $icon = 'far fa-bell';
+        } else {
+            $colour = 'gray';
+            $icon = 'fas fa-info';
+        }
         return '<div x-show="open" @click.away="open = false" class="fixed z-50 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
                         <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                             <div class="fixed inset-0 bg-gray-500 bg-opacity-75" aria-hidden="true" @click="open=false"></div>
@@ -43,9 +71,7 @@
                                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                                     <div class="sm:flex sm:items-start">
                                         <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-'.$colour.'-100 sm:mx-0 sm:h-10 sm:w-10">
-                                            <svg class="h-6 w-6 text-'.$colour.'-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-                                            </svg>
+                                            <i class="'.$icon.' text-'.$colour.'-500" aria-hidden="true"></i>
                                         </div>
                                         <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                                             <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
