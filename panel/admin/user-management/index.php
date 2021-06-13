c075fd16e1724bca7b1e2c11ac4a27ea5644ae83
lewmilburn <lewiscmilburn@gmail.com>
2021-06-13 21:39:34 +0100

    Fixed require and include links

--- panel/admin/user-management/index.php
+++ panel/admin/user-management/index.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    require_once __DIR__ . '/../../assets/common/global_private.php';
+    require_once __DIR__ . '/../../../assets/common/global_private.php';
 
     if(isset($_POST['action'])) {
         $userid = checkInput('DEFAULT', $_POST['userid']);
@@ -98,14 +98,14 @@
 ?><!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php include __DIR__ . '/../../assets/common/panel/vendors.php'; ?>
+        <?php include __DIR__ . '/../../../assets/common/panel/vendors.php'; ?>
 
         <title><?php echo CONFIG_SITE_NAME.' Admin Panel'; ?></title>
-        <?php require __DIR__ . '/../../assets/common/panel/theme.php'; ?>
+        <?php require __DIR__ . '/../../../assets/common/panel/theme.php'; ?>
 
     </head>
     <body class="bg-gray-200">
-        <?php require __DIR__ . '/../../assets/common/admin/navigation.php'; ?>
+        <?php require __DIR__ . '/../../../assets/common/admin/navigation.php'; ?>
 
             <div class="px-8 py-4 block w-full">
                 <h1 class="text-gray-900 text-3xl">User Management</h1>
