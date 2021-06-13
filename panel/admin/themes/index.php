b84662a1527f3f9185413639ffd90e14daeaa51f
lewmilburn <lewiscmilburn@gmail.com>
2021-06-13 23:10:58 +0100

    Added ability to list installed themes.

--- panel/admin/themes/index.php
+++ panel/admin/themes/index.php
@@ -30,6 +30,33 @@
             ?>
             <br>
             <h2 class="text-gray-900 text-2xl mt-8">Installed Themes</h2>
+            <div class="my-6 flex space-x-6">
+                <?php
+                    $dirs = array_filter(glob(__DIR__.'/../../../themes/*'), 'is_dir');
+                    foreach ($dirs as $dir) {
+                        $json =  file_get_contents($dir.'/theme.json');
+                        $themeData = json_decode($json);
+
+                        $image = $themeData->{'theme'}->{'image'};
+                        if ($image == "") {
+                            $image = CONFIG_INSTALL_URL.'/assets/images/no-image-500x500.png';
+                        }
+
+                        echo '<div class="bg-white w-52 h-52 relative shadow-lg hover:shadow-2xl transition duration-200">
+                            <div class="absolute bottom-0 w-full h-12 bg-black bg-opacity-50 overflow-x-auto">
+                                <h3 class="text-lg my-2 mx-2 text-white">'.$themeData->{'theme'}->{'name'}.'</h3>
+                            </div>
+                            <div class="absolute top-0 left rounded-br-md p-1 bg-black bg-opacity-50 text-white">
+                                <img src="'.CONFIG_INSTALL_URL.'/assets/images/icons/'.$themeData->{'theme'}->{'framework'}.'.svg" class="w-6 h-6">
+                            </div>
+                            <div class="absolute top-0 right-0 rounded-bl-md p-1 bg-black bg-opacity-50 text-white">
+                                '.$themeData->{'theme'}->{'version'}.'
+                            </div>
+                            <img class="w-full h-full" src="'.$image.'">
+                        </div>';
+                    }
+                ?>
+            </div>
         </div>
     </body>
 </html>
