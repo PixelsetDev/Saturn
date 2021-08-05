c56e8bcb5b2f1b9b677ec9203fdbf2a2b613cb30
Lew Milburn <lewiscmilburn@gmail.com>
2021-08-05 19:56:51 +0100

    Fixed CCV error message reappearing when cleared issue.

--- panel/admin/security/index.php
+++ panel/admin/security/index.php
@@ -51,7 +51,7 @@ ob_end_flush();
             <h2 class="text-gray-900 text-2xl mt-8">Core Checksum Validation</h2>
             <?php
             $issue = 0;
-            if (!ccv_validate('CONFIG')) {
+            if (!ccv_validate('CONFIG') && !isset($_GET['successResetCCV'])) {
                 echo '<br><div class="duration-300 transform bg-yellow-100 border-l-4 border-yellow-500 hover:-translate-y-2">
                 <div class="p-5 border border-l-0 rounded-r shadow-sm">
                     <h6 class="mb-2 font-semibold leading-5">[WARNING] Website configuration does not match checksum. <a href="https://docs.saturncms.net/website-configuration-checksum" class="underline text-xs text-black" target="_blank">Get help.</a></h6>
