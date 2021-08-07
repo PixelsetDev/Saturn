9d682836b20b7b8e4522507f4fed34c94b88fe98
lewmilburn <lewiscmilburn@gmail.com>
2021-08-07 19:22:34 +0100

    Added user settings get.

--- assets/common/processes/database/get/user_settings.php
+++ assets/common/processes/database/get/user_settings.php
@@ -1,1 +1,28 @@
 <?php
+    function get_user_settings_notifications_saturn($id)
+    {
+        $id = checkInput('DEFAULT', $id);
+
+        global $conn;
+
+        $query = 'SELECT `notifications_saturn` FROM `'.DATABASE_PREFIX.'users_settings` WHERE `id` = '.$id;
+
+        $rs = mysqli_query($conn, $query);
+        $row = mysqli_fetch_assoc($rs);
+
+        return $row['notifications_saturn'];
+    }
+
+    function get_user_settings_notifications_email($id)
+    {
+        $id = checkInput('DEFAULT', $id);
+
+        global $conn;
+
+        $query = 'SELECT `notifications_email` FROM `'.DATABASE_PREFIX.'users_settings` WHERE `id` = '.$id;
+
+        $rs = mysqli_query($conn, $query);
+        $row = mysqli_fetch_assoc($rs);
+
+        return $row['notifications_saturn'];
+    }
