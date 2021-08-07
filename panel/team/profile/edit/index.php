61226ff1a187b158c090427436cf1403c50bceb2
lewmilburn <lewiscmilburn@gmail.com>
2021-08-07 18:59:06 +0100

    Fixed user profile editing.

--- panel/team/profile/edit/index.php
+++ panel/team/profile/edit/index.php
@@ -1,19 +1,35 @@
-<?php session_start(); ?><h1 class="text-4xl">THIS ISN'T WORKING YET, I'LL SORT IT SOON :)</h1><!DOCTYPE html>
+<?php session_start();
+
+include_once __DIR__.'/../../../../assets/common/global_private.php';
+$user = $_SESSION['id'];
+
+if(isset($_POST['save'])) {
+    $bio = str_ireplace('\\', '', $_POST['bio']); // Fixes issue of many backwards slashes appearing.
+    $bio = checkInput('DEFAULT', $bio);
+    $link = checkInput('DEFAULT', $_POST['link']);
+    $fullname = checkInput('DEFAULT', $_POST['fullname']);
+    $namearray = explode(' ', $fullname, 2);
+
+    update_user_bio($user,$bio);
+    update_user_website($user,$link);
+    update_user_firstname($user,$namearray[0]);
+    update_user_lastname($user,$namearray[1]);
+}
+
+?><!DOCTYPE html>
 <html lang="en">
     <head>
         <?php
-            include_once __DIR__.'/../../../../assets/common/global_private.php';
             include_once __DIR__.'/../../../../assets/common/panel/vendors.php';
             include_once __DIR__.'/../../../../assets/common/panel/theme.php';
-            $user = $_SESSION['id'];
         ?>
 
-        <title><?php echo get_user_fullname($user); ?>'s Profile - Saturn Panel</title>
+        <title>Edit <?php echo get_user_fullname($user); ?>'s Profile - Saturn Panel</title>
 
     </head>
     <body class="mb-8">
         <?php include_once __DIR__.'/../../../../assets/common/panel/navigation.php'; ?>
-        <form class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <form class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8" method="post" action="index.php">
             <div class="w-full h-48" style="background: url('<?php echo CONFIG_INSTALL_URL; ?>/assets/panel/images/background.jpg');">
                 <div class="max-w-7xl flex mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
                     <div class="h-32 w-32 py-2 px-2 md:h-48 md:w-48 md:py-4 md:px-4 relative inline-block">
@@ -30,7 +46,7 @@
                             </div>
                         </div>
                         <?php if ($user == $_SESSION['id']) {
-            echo'<input type="submit" id="submit" name="submit" value="Save Profile" class="cursor-pointer h-7 px-3 ml-3 outline-none border-transparent text-center rounded border bg-blue-500 hover:bg-blue-600 text-white bg-transparent font-semibold">';
+            echo'<input type="submit" id="save" name="save" value="Save Profile" class="cursor-pointer h-7 px-3 ml-3 outline-none border-transparent text-center rounded border bg-blue-500 hover:bg-blue-600 text-white bg-transparent font-semibold">';
         } ?>
                     </div>
                 </div>
@@ -52,7 +68,7 @@
                     </ul>
                     <br>
                     <div class="">
-                        <div class="flex"><input type="text" id="bio" name="bio" value="<?php echo get_user_bio($user); ?>" class="text-base bg-gray-100 bg-opacity-50" maxlength="100"/><i class="fas fa-pencil-alt fa-lg text-black" aria-hidden="true"></i></div>
+                        <div class="flex"><input type="text" id="bio" name="bio" value="<?php echo str_ireplace('\\', '', get_user_bio($user)); ?>" class="text-base bg-gray-100 bg-opacity-50" maxlength="100"/><i class="fas fa-pencil-alt fa-lg text-black" aria-hidden="true"></i></div>
                         <div class="flex"><input type="text" id="link" name="link" value="<?php echo get_user_website($user); ?>" class="block text-base text-blue-500 mt-2 bg-gray-100 bg-opacity-50" /><i class="fas fa-pencil-alt fa-lg text-black" aria-hidden="true"></i></div>
                     </div>
                 </div>
