f9426dc86ba8f7bd988bfd4aa811b2208e6141ca
lewmilburn <lewiscmilburn@gmail.com>
2021-08-07 19:42:01 +0100

    Added get privacy (abbreviate surname) function.

--- assets/common/processes/database/get/user_settings.php
+++ assets/common/processes/database/get/user_settings.php
@@ -26,3 +26,17 @@
 
         return $row['notifications_email'];
     }
+
+    function get_user_settings_privacy_abbreviate_surname($id)
+    {
+        $id = checkInput('DEFAULT', $id);
+
+        global $conn;
+
+        $query = 'SELECT `privacy_abbreviate_surname` FROM `'.DATABASE_PREFIX.'users_settings` WHERE `id` = '.$id;
+
+        $rs = mysqli_query($conn, $query);
+        $row = mysqli_fetch_assoc($rs);
+
+        return $row['privacy_abbreviate_surname'];
+    }
