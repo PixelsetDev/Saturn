578613c2da3cc5c571e6b151fcf1f082647ab7b5
lewmilburn <lewiscmilburn@gmail.com>
2021-08-08 10:43:11 +0100

    Fixed accessibility issue, added title to iframe.

--- panel/team/profile/index.php
+++ panel/team/profile/index.php
@@ -87,7 +87,7 @@
                 foreach ($resultset as $result) {
                     ?>
                 <a href="<?php echo $result[1]; ?>" target="_blank" class="relative flex-none" style="width:300px; height:300px">
-                    <iframe src="<?php echo $result[1]; ?>?access_method=saturn_iframe_preview" class="cursor-pointer relative flex-none" style="width:300px; height:300px"></iframe>
+                    <iframe src="<?php echo $result[1]; ?>?access_method=saturn_iframe_preview" class="cursor-pointer relative flex-none" title="Preview of Page Titled <?php echo $result[0]; ?>" style="width:300px; height:300px"></iframe>
                     <div class="absolute bottom-0 left-0 w-full bg-black bg-opacity-25 px-2 py-1"><?php echo $result[0]; ?></div>
                 </a>
                 <?php
