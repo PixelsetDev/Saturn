08f186b2f4071bca25e1701a169e1b00003428dd
lewmilburn <lewmilburn@users.noreply.github.com>
2021-08-24 10:16:09 +0000

    Apply fixes from StyleCI

--- assets/common/processes/database/get/activity.php
+++ assets/common/processes/database/get/activity.php
@@ -2,8 +2,9 @@
 
     function get_activity($id): string
     {
-        $date = strtotime("2012-03-08 16:02:35");
+        $date = strtotime('2012-03-08 16:02:35');
         $now = time();
+
         return $now - $date;
     }
 
