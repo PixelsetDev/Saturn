e2129d6cafefe6b6b705fb8edcbf7989e86921a2
Lew Milburn <lewiscmilburn@gmail.com>
2021-08-24 10:58:08 +0100

    Abstracted render engine into functions.

--- assets/common/processes/render_engine/pages.php
+++ assets/common/processes/render_engine/pages.php
@@ -15,24 +15,29 @@ function get_page_id_from_url($uri)
     return $row['id'];
 }
 
+function getdata($pageID): array {
+    $pageData['title'] = get_page_title($pageID);
+    $pageData['content'] = get_page_content($pageID);
+    $pageData['author']['id'] = get_page_last_edit_user_id($pageID);
+    $pageData['section']['navigation'] = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/themes/'.THEME_SLUG.'/navigation.template');
+    $pageData['section']['footer'] = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/themes/'.THEME_SLUG.'/footer.template');
+
+    return $pageData;
+}
+
+function replacedata($pageOutput, $pageData): string {
+    $pageOutput = str_replace('{{data:title}}', $pageData['title'], $pageOutput);
+    $pageOutput = str_replace('{{data:content}}', $pageData['content'], $pageOutput);
+    $pageOutput = str_replace('{{data:author:name}}', $pageData['author']['id'], $pageOutput);
+    $pageOutput = str_replace('{{section:navigation}}', $pageData['section']['navigation'], $pageOutput);
+    return str_replace('{{section:footer}}', $pageData['section']['footer'], $pageOutput);
+}
+
 $pageID = get_page_id_from_url($pageuri);
 
 $data = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/themes/'.THEME_SLUG.'/theme.json'));
-
 $file = $data->theme->templates->DEFAULT->file;
-
 $pageOutput = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/themes/'.THEME_SLUG.'/'.$file);
 
-$pageData['title'] = get_page_title($pageID);
-$pageData['content'] = get_page_content($pageID);
-$pageData['author']['id'] = get_page_last_edit_user_id($pageID);
-$pageData['section']['navigation'] = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/themes/'.THEME_SLUG.'/navigation.template');
-$pageData['section']['footer'] = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/themes/'.THEME_SLUG.'/footer.template');
-
-$pageOutput = str_replace('{{data:title}}', $pageData['title'], $pageOutput);
-$pageOutput = str_replace('{{data:content}}', $pageData['content'], $pageOutput);
-$pageOutput = str_replace('{{data:author:name}}', $pageData['author']['id'], $pageOutput);
-$pageOutput = str_replace('{{section:navigation}}', $pageData['section']['navigation'], $pageOutput);
-$pageOutput = str_replace('{{section:footer}}', $pageData['section']['footer'], $pageOutput);
-
-echo $pageOutput;
+$pageData = getdata($pageID);
+echo replacedata($pageOutput, $pageData);
