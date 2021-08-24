bfd69ad1f063656a26425844f7950ba3ebe839e1
Lew Milburn <lewiscmilburn@gmail.com>
2021-08-24 15:31:33 +0100

    Fixed attempting to destroy un-set session.

--- panel/account/signout/index.php
+++ panel/account/signout/index.php
@@ -1,6 +1,4 @@
 <?php
-include_once __DIR__.'/../../../assets/common/global_public.php';
-
 session_start();
 session_destroy();
 
@@ -10,6 +8,7 @@ session_destroy();
     <head>
         <title>Sign out - Saturn Panel</title>
         <?php
+        include_once __DIR__.'/../../../assets/common/global_public.php';
         include_once __DIR__.'/../../../assets/common/panel/vendors.php';
         include_once __DIR__.'/../../../assets/common/panel/theme.php';
         ?>
