d8cf4e3e66ce89a2fa333579a2bebaf1ed21f4ab
Lew Milburn <lewiscmilburn@gmail.com>
2021-09-04 20:11:52 +0100

    Fixed accessibility issues.

--- panel/account/signin/forgot-password/index.php
+++ panel/account/signin/forgot-password/index.php
@@ -158,7 +158,7 @@
                             echo '<div>
                             <button type="submit" name="login" class="hover:shadow-lg group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-all duration-200">
                                 <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-                                    <i class="fas fa-unlock"></i>
+                                    <i class="fas fa-unlock" aria-hidden="true"></i>
                                 </span>
                                 Reset Password
                             </button>
@@ -166,7 +166,7 @@
                         } else {
                             echo '<a href="'.CONFIG_INSTALL_URL.'/panel/account/signin" class="hover:shadow-lg group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-all duration-200">
                                 <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-                                    <i class="fas fa-lock"></i>
+                                    <i class="fas fa-lock" aria-hidden="true"></i>
                                 </span>
                                 Sign in
                             </a>';
