42add14f34b02072241c6481e34e18c0fc1c70db
Lew Milburn <lewiscmilburn@gmail.com>
2021-09-05 23:30:54 +0100

    Updated GUI Alert components.

--- assets/common/processes/gui/alerts.php
+++ assets/common/processes/gui/alerts.php
@@ -15,33 +15,38 @@ function alert($type, $message): bool
 
     if ($type == 'ERROR') {
         echo '<br>
-                            <div class="duration-300 transform bg-red-100 border-l-4 border-red-500 hover:-translate-y-2">
-                                <div class="p-5 border border-l-0 rounded-r shadow-sm">
-                                    <h6 class="mb-2 font-semibold leading-5">[ERROR] '.$message.'</h6>
+                            <div class="duration-300 transform bg-red-50 border-l-4 border-red-500 hover:-translate-y-2">
+                                <div class="p-2 border border-l-0 rounded-r shadow-sm flex">
+                                    <i class="px-3 fas fa-times text-red-500 fa-2x self-center" aria-hidden="true"></i>
+                                    <h6 class="p-3 font-semibold leading-5">'.$message.'</h6>
                                 </div>
                             </div>';
     } elseif ($type == 'WARNING') {
-        echo '<br><div class="duration-300 transform bg-yellow-100 border-l-4 border-yellow-500 hover:-translate-y-2">
-                                <div class="p-5 border border-l-0 rounded-r shadow-sm">
-                                    <h6 class="mb-2 font-semibold leading-5">[WARNING] '.$message.'</h6>
+        echo '<br><div class="duration-300 transform bg-yellow-50 border-l-4 border-yellow-500 hover:-translate-y-2">
+                                <div class="p-2 border border-l-0 rounded-r shadow-sm flex">
+                                    <i class="px-2 fas fa-exclamation-triangle text-yellow-500 fa-2x self-center" aria-hidden="true"></i>
+                                    <h6 class="p-3 font-semibold leading-5">'.$message.'</h6>
                                 </div>
                             </div>';
     } elseif ($type == 'SUCCESS') {
-        echo '<br><div class="duration-300 transform bg-green-100 border-l-4 border-green-500 hover:-translate-y-2">
-                                <div class="p-5 border border-l-0 rounded-r shadow-sm">
-                                    <h6 class="mb-2 font-semibold leading-5">'.$message.'</h6>
+        echo '<br><div class="duration-300 transform bg-green-50 border-l-4 border-green-500 hover:-translate-y-2">
+                                <div class="p-2 border border-l-0 rounded-r shadow-sm flex">
+                                    <i class="px-2 fas fa-check-circle text-green-500 fa-2x self-center" aria-hidden="true"></i>
+                                    <h6 class="p-3 font-semibold leading-5">'.$message.'</h6>
                                 </div>
                             </div>';
     } elseif ($type == 'INFO') {
-        echo '<br><div class="duration-300 transform bg-blue-100 border-l-4 border-blue-500 hover:-translate-y-2">
-                                <div class="p-5 border border-l-0 rounded-r shadow-sm">
-                                    <h6 class="mb-2 font-semibold leading-5">'.$message.'</h6>
+        echo '<br><div class="duration-300 transform bg-blue-50 border-l-4 border-blue-500 hover:-translate-y-2">
+                                <div class="p-2 border border-l-0 rounded-r shadow-sm flex">
+                                    <i class="px-5 fas fa-info text-blue-500 fa-2x self-center" aria-hidden="true"></i>
+                                    <h6 class="p-3 font-semibold leading-5">'.$message.'</h6>
                                 </div>
                             </div>';
     } else {
-        echo '<br><div class="duration-300 transform bg-gray-100 border-l-4 border-gray-500 hover:-translate-y-2">
-                                <div class="p-5 border border-l-0 rounded-r shadow-sm">
-                                    <h6 class="mb-2 font-semibold leading-5">'.$message.'</h6>
+        echo '<br><div class="duration-300 transform bg-gray-50 border-l-4 border-gray-500 hover:-translate-y-2">
+                                <div class="p-2 border border-l-0 rounded-r shadow-sm flex">
+                                    <i class="px-2 fas fa-bell text-gray-500 fa-2x self-center" aria-hidden="true"></i>
+                                    <h6 class="p-3 font-semibold leading-5">'.$message.'</h6>
                                 </div>
                             </div>';
     }
@@ -63,33 +68,38 @@ function static_alert($type, $message): bool
     $message = stripslashes($message);
 
     if ($type == 'ERROR') {
-        echo '<div class="bg-red-100 border-l-4 border-red-500">
-                                <div class="p-5 border border-l-0 rounded-r shadow-sm">
-                                    <h6 class="mb-2 font-semibold leading-5">[ERROR] '.$message.'</h6>
+        echo '<div class="bg-red-50 border-l-4 border-red-500">
+                                <div class="p-2 border border-l-0 rounded-r shadow-sm flex">
+                                    <i class="px-3 fas fa-times text-red-500 fa-2x self-center" aria-hidden="true"></i>
+                                    <h6 class="p-3 font-semibold leading-5">'.$message.'</h6>
                                 </div>
                             </div>';
     } elseif ($type == 'WARNING') {
-        echo '<div class="bg-yellow-100 border-l-4 border-yellow-500">
-                                <div class="p-5 border border-l-0 rounded-r shadow-sm">
-                                    <h6 class="mb-2 font-semibold leading-5">[WARNING] '.$message.'</h6>
+        echo '<div class="bg-yellow-50 border-l-4 border-yellow-500">
+                                <div class="p-2 border border-l-0 rounded-r shadow-sm flex">
+                                    <i class="px-2 fas fa-exclamation-triangle text-yellow-500 fa-2x self-center" aria-hidden="true"></i>
+                                    <h6 class="p-3 font-semibold leading-5">'.$message.'</h6>
                                 </div>
                             </div>';
     } elseif ($type == 'SUCCESS') {
-        echo '<div class="bg-green-100 border-l-4 border-green-500">
-                                <div class="p-5 border border-l-0 rounded-r shadow-sm">
-                                    <h6 class="mb-2 font-semibold leading-5">'.$message.'</h6>
+        echo '<div class="bg-green-50 border-l-4 border-green-500">
+                                <div class="p-2 border border-l-0 rounded-r shadow-sm flex">
+                                    <i class="px-2 fas fa-check-circle text-green-500 fa-2x self-center" aria-hidden="true"></i>
+                                    <h6 class="p-3 font-semibold leading-5">'.$message.'</h6>
                                 </div>
                             </div>';
     } elseif ($type == 'INFO') {
-        echo '<div class="bg-blue-100 border-l-4 border-blue-500">
-                                <div class="p-5 border border-l-0 rounded-r shadow-sm">
-                                    <h6 class="mb-2 font-semibold leading-5">'.$message.'</h6>
+        echo '<div class="bg-blue-50 border-l-4 border-blue-500">
+                                <div class="p-2 border border-l-0 rounded-r shadow-sm flex">
+                                    <i class="px-5 fas fa-info text-blue-500 fa-2x self-center" aria-hidden="true"></i>
+                                    <h6 class="p-3 font-semibold leading-5">'.$message.'</h6>
                                 </div>
                             </div>';
     } else {
-        echo '<div class="bg-gray-100 border-l-4 border-gray-500">
-                                <div class="p-5 border border-l-0 rounded-r shadow-sm">
-                                    <h6 class="mb-2 font-semibold leading-5">'.$message.'</h6>
+        echo '<div class="bg-gray-50 border-l-4 border-gray-500">
+                                <div class="p-2 border border-l-0 rounded-r shadow-sm flex">
+                                    <i class="px-2 fas fa-bell text-gray-500 fa-2x self-center" aria-hidden="true"></i>
+                                    <h6 class="p-3 font-semibold leading-5">'.$message.'</h6>
                                 </div>
                             </div>';
     }
