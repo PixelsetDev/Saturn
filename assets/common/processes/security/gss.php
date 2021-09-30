5eed0c3761c39c08ebadbd563ebfc39f08935c54
lewmilburn <lewmilburn@users.noreply.github.com>
2021-09-30 15:02:46 +0000

    Apply fixes from StyleCI

--- assets/common/processes/security/gss.php
+++ assets/common/processes/security/gss.php
@@ -4,8 +4,8 @@
     {
         global $conn;
 
-        $reason = checkInput('DEFAULT',$reason);
-        $id = checkInput('DEFAULT',$id);
+        $reason = checkInput('DEFAULT', $reason);
+        $id = checkInput('DEFAULT', $id);
 
         $query = 'SELECT `last_login_ip` FROM `'.DATABASE_PREFIX.'users` WHERE `id` = '.$id;
 
@@ -14,10 +14,10 @@
 
         $ip = $row['last_login_ip'];
 
-        $query = "UPDATE `".DATABASE_PREFIX."users` SET `first_name`='Banned',`last_name`='User',`email`=NULL,`password`=NULL,`user_key`=NULL,`last_login_ip`=NULL,`auth_code`=NULL,`role_id`='0',`bio`=NULL,`organisation`=NULL,`website`=NULL,`profile_photo`='/assets/images/defaultprofile.png' WHERE id = ".$id;
+        $query = 'UPDATE `'.DATABASE_PREFIX."users` SET `first_name`='Banned',`last_name`='User',`email`=NULL,`password`=NULL,`user_key`=NULL,`last_login_ip`=NULL,`auth_code`=NULL,`role_id`='0',`bio`=NULL,`organisation`=NULL,`website`=NULL,`profile_photo`='/assets/images/defaultprofile.png' WHERE id = ".$id;
 
         if (SECURITY_USE_GSS) {
-            if (mysqli_query($conn, $query) && file_get_contents('https://link.saturncms.net/gss/?instruction=publish_ban&key=' . CONFIG_ACTIVATION_KEY . '&domain=' . $_SERVER['HTTP_HOST'] . '&ip=' . $ip . '&reason=' . $reason)) {
+            if (mysqli_query($conn, $query) && file_get_contents('https://link.saturncms.net/gss/?instruction=publish_ban&key='.CONFIG_ACTIVATION_KEY.'&domain='.$_SERVER['HTTP_HOST'].'&ip='.$ip.'&reason='.$reason)) {
                 return true;
             } else {
                 return false;
@@ -36,6 +36,7 @@
         if ($hashed == false) {
             $ip = hash_ip($ip);
         }
+
         return file_get_contents('https://link.saturncms.net/gss/?instruction=check_ban&key='.CONFIG_ACTIVATION_KEY.'&domain='.$_SERVER['HTTP_HOST'].'&ip='.$ip);
     }
 
