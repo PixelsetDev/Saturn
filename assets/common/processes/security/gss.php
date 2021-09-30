7ce0a2da764beb2ee31709f8caa51720ebbe1044
lewmilburn <lewiscmilburn@gmail.com>
2021-09-30 14:52:10 +0100

    Updated GSS banning system.

--- assets/common/processes/security/gss.php
+++ assets/common/processes/security/gss.php
@@ -1,9 +1,22 @@
 <?php
-    function ban_publish($ip, $reason): bool
+
+    function ban_user($id, $reason): bool
     {
+        global $conn;
+
         $reason = checkInput('DEFAULT',$reason);
-        $ip = checkInput('DEFAULT',$ip);
-        if (file_get_contents('https://link.saturncms.net/gss/?instruction=publish_ban&key='.CONFIG_ACTIVATION_KEY.'&domain='.$_SERVER['HTTP_HOST'].'&ip='.$ip.'&reason='.$reason)) {
+        $id = checkInput('DEFAULT',$id);
+
+        $query = 'SELECT `last_login_ip` FROM `'.DATABASE_PREFIX.'users` WHERE `id` = '.$id;
+
+        $rs = mysqli_query($conn, $query);
+        $row = mysqli_fetch_assoc($rs);
+
+        $ip = $row['last_login_ip'];
+
+        $query = "UPDATE `".DATABASE_PREFIX."users` SET `first_name`='Restricted',`last_name`='User',`email`=NULL,`password`=NULL,`user_key`=NULL,`last_login_ip`=NULL,`auth_code`=NULL,`role_id`='0',`bio`=NULL,`organisation`=NULL,`website`=NULL,`profile_photo`='/assets/images/defaultprofile.png' WHERE id = ".$id;
+
+        if (mysqli_query($conn, $query) && file_get_contents('https://link.saturncms.net/gss/?instruction=publish_ban&key='.CONFIG_ACTIVATION_KEY.'&domain='.$_SERVER['HTTP_HOST'].'&ip='.$ip.'&reason='.$reason)) {
             return true;
         } else {
             return false;
