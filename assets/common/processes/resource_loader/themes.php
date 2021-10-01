dae57018d68112c74874b4f3920b9d4f685a244a
lewmilburn <lewiscmilburn@gmail.com>
2021-10-01 12:41:15 +0100

    Changed theme loading system.

--- assets/common/processes/resource_loader/themes.php
+++ assets/common/processes/resource_loader/themes.php
@@ -1,21 +1,6 @@
 <?php
-
-    $foundTheme = false;
-    $prefix = 'Saturn][Resource Loader][Themes';
-    foreach (glob(dirname(__DIR__, 4).'/themes/*.php') as $filename) {
-        include_once $filename;
-        if (CONFIG_DEBUG === true) {
-            log_console($prefix, 'Loaded Theme: '.$filename.'.');
-        }
-        $foundTheme = true;
-    }
-    if ($foundTheme) {
-        if (CONFIG_DEBUG === true) {
-            log_console($prefix, 'Theme loading complete.');
-        }
-    } else {
-        if (CONFIG_DEBUG === true) {
-            log_console($prefix, 'No themes found.');
-        }
-    }
-    unset($foundTheme, $filename);
+$themeDataJSON = file_get_contents(__DIR__.'/../../../../themes/'.THEME_SLUG.'/theme.json');
+$themeData = json_decode($themeDataJSON);
+if ($themeData->{'theme'}->{'name'} != null || $themeData->{'theme'}->{'name'} != '') {
+    log_console('SATURN][RESOURCE LOADER][THEMES','Loaded theme: '.$themeData->{'theme'}->{'name'});
+}
