c61fcd2a5a73c3749a2b1c0c80152679cb0abd4b
lewmilburn <lewiscmilburn@gmail.com>
2021-10-01 12:04:43 +0100

    Updated theme config.

--- theme.php
+++ theme.php
@@ -1,10 +1,10 @@
 <?php
-
     /* Website Theme */
     const THEME_SLUG = 'andromeda';
+    const THEME_COLOUR_SCHEME = 'andromeda';
+    const THEME_FONT = 'Questrial'; /* Google Font Name */
     /* Panel Theme */
     const THEME_PANEL_FONT = 'Questrial'; /* Google Font Name */
     const THEME_PANEL_COLOUR = 'gray';
     /* Site Branding */
-    const THEME_PANEL_BACKGROUND_IMAGE = CONFIG_INSTALL_URL.'/assets/panel/images/background.jpg';
     const THEME_SOCIAL_IMAGE = CONFIG_INSTALL_URL.'/assets/images/social-image.jpg';
