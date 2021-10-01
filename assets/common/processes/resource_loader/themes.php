8130aad71a7d93877cc99f2b5b199489b10592a6
lewmilburn <lewiscmilburn@gmail.com>
2021-10-01 13:42:13 +0100

    Modified themes resource loader.

--- assets/common/processes/resource_loader/themes.php
+++ assets/common/processes/resource_loader/themes.php
@@ -1,6 +1,9 @@
 <?php
-$themeDataJSON = file_get_contents(__DIR__.'/../../../../themes/'.THEME_SLUG.'/theme.json');
-$themeData = json_decode($themeDataJSON);
-if ($themeData->{'theme'}->{'name'} != null || $themeData->{'theme'}->{'name'} != '') {
-    log_console('SATURN][RESOURCE LOADER][THEMES','Loaded theme: '.$themeData->{'theme'}->{'name'});
-}
+    $themeDataJSON = file_get_contents(__DIR__ . '/../../../../themes/' . THEME_SLUG . '/theme.json');
+    $themeData = json_decode($themeDataJSON);
+    if ($themeData->{'theme'}->{'name'} != null || $themeData->{'theme'}->{'name'} != '') {
+        if (CONFIG_DEBUG) {log_console('SATURN][RESOURCE LOADER][THEMES', 'Loaded theme: ' . $themeData->{'theme'}->{'name'}); }
+    } else {
+        if (CONFIG_DEBUG) {log_console('SATURN][RESOURCE LOADER][THEMES', 'Unable to load theme: ' . THEME_SLUG); }
+        echo alert('ERROR','Unable to load theme: ' . THEME_SLUG);
+    }
