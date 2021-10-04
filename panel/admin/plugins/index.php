d445006e524caf22616b721c54cf69ed2f12e104
lewmilburn <lewiscmilburn@gmail.com>
2021-10-04 12:59:09 +0100

    Added download script.

--- panel/admin/plugins/index.php
+++ panel/admin/plugins/index.php
@@ -4,7 +4,50 @@
     ob_start();
     require_once __DIR__.'/../../../assets/common/global_private.php';
     require_once __DIR__.'/../../../assets/common/admin/global.php';
+
+    if (isset($_GET['download'])) {
+        $download_url = $_GET['download'];
+        $delete = true;
+
+        $file = __DIR__."/../../../plugins/download.zip";
+        $script = basename($_SERVER['PHP_SELF']);
+
+        // Download file
+        file_put_contents($file, fopen($download_url, 'r'));
+
+        // Extract
+        $path = pathinfo(realpath($file), PATHINFO_DIRNAME);
+
+        $zip = new ZipArchive;
+        $res = $zip->open($file);
+
+        if ($res) {
+            $zip->extractTo($path);
+            $zip->close();
+
+            $successMsg = "The plugin was downloaded and installed successfully.";
+            if ($delete) {
+                if (!unlink($file)) {
+                    $warningMsg = 'Archive was extracted but not deleted. This could mean the plugin was not downloaded.';
+                    internal_redirect('/panel/admin/plugins?successMsg='.$successMsg.'&warningMsg='.$warningMsg);
+                }
+            } else {
+                $warningMsg = 'Archive was extracted but not deleted.';
+                internal_redirect('/panel/admin/plugins?successMsg='.$successMsg.'&warningMsg='.$warningMsg);
+            }
+            internal_redirect('/panel/admin/plugins?successMsg='.$successMsg);
+        } else {
+            $errorMsg = "Plugin could not be downloaded. Couldn't open $file.";
+            internal_redirect('/panel/admin/plugins?errorMsg='.$errorMsg);
+        }
+        exit;
+    }
+
     ob_end_flush();
+
+    if (isset($_GET['successMsg'])) { $successMsg = checkInput('DEFAULT', $_GET['successMsg']); }
+    if (isset($_GET['warningMsg'])) { $warningMsg = checkInput('DEFAULT', $_GET['warningMsg']); }
+    if (isset($_GET['errorMsg'])) { $errorMsg = checkInput('DEFAULT', $_GET['errorMsg']); }
 ?><!DOCTYPE html>
 <html lang="en">
     <head>
