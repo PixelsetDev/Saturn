b3e935f65bc6daf13e7ec8e975a564f63813e1c8
lewmilburn <lewiscmilburn@gmail.com>
2021-10-04 15:23:36 +0100

    Added protection against attacks.

--- assets/common/processes/redirect.php
+++ assets/common/processes/redirect.php
@@ -2,6 +2,7 @@
 
     function internal_redirect($url)
     {
+        $url = checkInput('DEFAULT', $url);
         header('Location: '.CONFIG_INSTALL_URL.$url);
         echo '<meta http-equiv="refresh" content="0; url='.CONFIG_INSTALL_URL.$url.'">';
         exit;
@@ -9,6 +10,7 @@
 
     function redirect($url)
     {
+        $url = checkInput('DEFAULT', $url);
         header('Location: '.$url);
         echo '<meta http-equiv="refresh" content="0; url='.$url.'">';
         exit;
