70b8fef6e571b4599f1dd18b8048d47bd3507841
lewmilburn <lewiscmilburn@gmail.com>
2021-10-05 00:09:36 +0100

    Refactored code.

--- assets/common/processes/database/get/page_category.php
+++ assets/common/processes/database/get/page_category.php
@@ -1,5 +1,31 @@
 <?php
 
+    function get_page_category_name($id)
+    {
+        $id = checkInput('DEFAULT', $id);
+
+        global $conn;
+
+        $query = 'SELECT `name` FROM `'.DATABASE_PREFIX.'pages_categories` WHERE `id` = '.$id;
+        $rs = mysqli_query($conn, $query);
+        $row = mysqli_fetch_assoc($rs);
+
+        return $row['name'];
+    }
+
+    function get_page_category_id($id): string
+    {
+        $id = checkInput('DEFAULT', $id);
+
+        global $conn;
+
+        $query = 'SELECT `category_id` FROM `'.DATABASE_PREFIX.'pages` WHERE `id` = '.$id;
+        $rs = mysqli_query($conn, $query);
+        $row = mysqli_fetch_assoc($rs);
+
+        return $row['category_id'];
+    }
+
     function get_page_category_homepage($id)
     {
         $id = checkInput('DEFAULT', $id);
