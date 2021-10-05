fa51c1c3a4840cd5da683e551b82c981c54d0590
lewmilburn <lewiscmilburn@gmail.com>
2021-10-05 14:31:39 +0100

    Made error messages look nicer and actually give relevant information.

--- assets/common/processes/database/connect.php
+++ assets/common/processes/database/connect.php
@@ -2,12 +2,35 @@
 
     $conn = mysqli_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME, DATABASE_PORT);
     if (mysqli_connect_errno($conn)) {
-        if (CONFIG_DEBUG === true) {
-            errorHandlerError('x', 'Failed to connect to MySQL Database. Please check your config.php file and try again. Error: '.mysqli_connect_error());
-        }
-        errorHandlerRedirect('db_conn');
+        ?>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        <img src="/assets/images/logo.png" class="mx-auto w-1/6">
+        <p class="w-full text-center text-xl font-bold">Saturn Error Recovery.</p>
+        <p class="w-full text-center">Sorry, an fatal error has occurred whilst trying to display this page.</p>
+        <?php if (!CONFIG_DEBUG) { ?>
+        <div class="duration-300 transform bg-red-100 border-l-4 border-red-500 mt-32 mx-32">
+            <div class="h-auto p-5 border border-l-0 rounded-r shadow-sm">
+                <h6 class="mb-2 font-semibold leading-5">[<?php date('H:i') ?>]
+                <span class="font-medium">Failed to connect to MySQL Database. Please check your website's configuration file and try again. Enable debug mode for more information.</span></h6>
+            </div>
+        </div>
+        <?php } else { ?>
+        <div class="duration-300 transform bg-red-100 border-l-4 border-red-500 mt-32 mx-32">
+            <div class="h-auto p-5 border border-l-0 rounded-r shadow-sm">
+                <h6 class="mb-2 font-semibold leading-5">[<?php date('H:i') ?>]
+                <span class="font-medium">Failed to connect to MySQL Database. Please check your website's configuration file and try again. More information below.</span></h6>
+            </div>
+        </div>
+        <p class="bg-black text-white p-2 rounded w-auto mt-8 mx-32">
+            MYSQLI_CONNECT_ERRNO(<?php echo mysqli_connect_errno($conn); ?>) - <?php echo mysqli_connect_error($conn); ?>
+        </p>
+        <?php } ?>
+        <a href="https://saturncms.net" target="_blank"><img src="/assets/panel/images/saturn.png" class="mx-auto w-32 mt-96"></a>
+        <p class="w-full text-center text-xs">Powered by Saturn</p>
+<?php
+        exit;
+    } else {
+        $query = "SET time_zone = '".CONFIG_SITE_TIMEZONE."';";
+        $rs = mysqli_query($conn, $query);
+        unset($query,$rs);
     }
-
-    $query = "SET time_zone = '".CONFIG_SITE_TIMEZONE."';";
-    $rs = mysqli_query($conn, $query);
-    unset($query,$rs);
