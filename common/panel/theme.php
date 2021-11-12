a6cd952a0df9c98f08b88581cdbf1e669b907ef1
lewmilburn <lewiscmilburn@gmail.com>
2021-11-12 15:00:41 +0000

    Updated includes.

--- common/panel/theme.php
+++ common/panel/theme.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__.'/../../../theme.php';
+    require_once __DIR__.'/../../theme.php';
 ?><style>
     @import url('https://fonts.googleapis.com/css2?family=<?php echo THEME_PANEL_FONT; ?>:wght@400&display=swap');
     * {
