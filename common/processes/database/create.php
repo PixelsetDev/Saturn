b26eafa5161ff5179267f93a4b4bb78765db0275
lewmilburn <lewiscmilburn@gmail.com>
2021-11-12 12:10:25 +0000

    Added create articles.

--- common/processes/database/create.php
+++ common/processes/database/create.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__.'/create/article.php';
 require_once __DIR__.'/create/notification.php';
 require_once __DIR__.'/create/user.php';
 require_once __DIR__.'/create/page.php';
