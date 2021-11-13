80315a6c781922d811c425228039308fb5ddaca5
lewmilburn <lewiscmilburn@gmail.com>
2021-11-13 00:39:44 +0000

    Added create category.

--- common/processes/database/create/page.php
+++ common/processes/database/create/page.php
@@ -1,23 +1,41 @@
 <?php
 
-    function create_page($url, $categoryID, $template, $title, $description): bool
-    {
-        $url = checkInput('DEFAULT', $url);
-        $categoryID = checkInput('DEFAULT', $categoryID);
-        $template = checkInput('DEFAULT', $template);
-        $title = checkInput('DEFAULT', $title);
-        $description = checkInput('DEFAULT', $description);
+function create_page($url, $categoryID, $template, $title, $description): bool
+{
+    $url = checkInput('DEFAULT', $url);
+    $categoryID = checkInput('DEFAULT', $categoryID);
+    $template = checkInput('DEFAULT', $template);
+    $title = checkInput('DEFAULT', $title);
+    $description = checkInput('DEFAULT', $description);
 
-        global $conn;
+    global $conn;
 
-        $query = 'INSERT INTO `'.DATABASE_PREFIX."pages` (`id`, `user_id`, `category_id`, `url`, `template`, `title`, `description`, `content`, `reference`) VALUES (NULL, '".$_SESSION['id']."', '".$categoryID."', '".$url."', '".$template."', '".$title."', '".$description."', NULL, NULL)";
-        $query2 = 'INSERT INTO `'.DATABASE_PREFIX."pages_pending` (`id`, `user_id`, `category_id`, `title`, `content`, `reference`) VALUES (NULL, '0', '".$categoryID."', NULL, NULL, NULL)";
+    $query = 'INSERT INTO `'.DATABASE_PREFIX."pages` (`id`, `user_id`, `category_id`, `url`, `template`, `title`, `description`, `content`, `reference`) VALUES (NULL, '".$_SESSION['id']."', '".$categoryID."', '".$url."', '".$template."', '".$title."', '".$description."', NULL, NULL)";
+    $query2 = 'INSERT INTO `'.DATABASE_PREFIX."pages_pending` (`id`, `user_id`, `category_id`, `title`, `content`, `reference`) VALUES (NULL, '0', '".$categoryID."', NULL, NULL, NULL)";
 
-        if (mysqli_query($conn, $query) && mysqli_query($conn, $query2)) {
-            $return = true;
-        } else {
-            $return = false;
-        }
-
-        return $return;
+    if (mysqli_query($conn, $query) && mysqli_query($conn, $query2)) {
+        $return = true;
+    } else {
+        $return = false;
     }
+
+    return $return;
+}
+
+function create_category($title, $homepage): bool
+{
+    $title = checkInput('DEFAULT', $title);
+    $homepage = checkInput('DEFAULT', $homepage);
+
+    global $conn;
+
+    $query = "INSERT INTO `".DATABASE_PREFIX."pages_categories` (`id`, `name`, `homepage_id`) VALUES (NULL, '".$title."', '".$homepage."')";
+
+    if (mysqli_query($conn, $query)) {
+        $return = true;
+    } else {
+        $return = false;
+    }
+
+    return $return;
+}
