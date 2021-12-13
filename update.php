d64fc317b4278b1402e1818ce57b579b613376d5
lewmilburn <lewmilburn@users.noreply.github.com>
2021-12-13 22:02:58 +0000

    Apply fixes from StyleCI

--- update.php
+++ update.php
@@ -13,15 +13,17 @@ foreach ($items as $item) {
     }
 }
 
-function remove($dir) {
+function remove($dir)
+{
     if (is_dir($dir)) {
         $objects = scandir($dir);
         foreach ($objects as $object) {
-            if ($object != "." && $object != "..") {
-                if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object))
-                    remove($dir. DIRECTORY_SEPARATOR .$object);
-                else
-                    unlink($dir. DIRECTORY_SEPARATOR .$object);
+            if ($object != '.' && $object != '..') {
+                if (is_dir($dir.DIRECTORY_SEPARATOR.$object) && !is_link($dir.'/'.$object)) {
+                    remove($dir.DIRECTORY_SEPARATOR.$object);
+                } else {
+                    unlink($dir.DIRECTORY_SEPARATOR.$object);
+                }
             }
         }
         rmdir($dir);
