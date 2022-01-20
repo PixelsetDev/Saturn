cf710691a6ace1646c9e5d7d9c71832d18e9aa5a
lewmilburn <lewiscmilburn@gmail.com>
2022-01-20 18:43:18 +0000

    Updated RERL debug text.

--- common/processes/resource_loader/translation.php
+++ common/processes/resource_loader/translation.php
@@ -5,13 +5,13 @@
     if ($lang->{'language'} != null || $lang->{'language'} != '') {
         if (CONFIG_DEBUG) {
             if ($lang->{'version'} != SATURN_VERSION) {
-                log_console('SATURN][RESOURCE LOADER][TRANSLATION', 'WARNING: Language file '.$lang->{'language'}.' is for Saturn version '.$lang->{'version'}.' - you are running version '.SATURN_VERSION);
+                log_console('SATURN][RERL][TRANSLATION', 'WARNING: Language file '.$lang->{'language'}.' is for Saturn version '.$lang->{'version'}.' - you are running version '.SATURN_VERSION);
             }
-            log_console('SATURN][RESOURCE LOADER][TRANSLATION', 'Loaded theme: '.$lang->{'theme'}->{'name'});
+            log_console('SATURN][RERL][TRANSLATION', 'Loaded language file: '.$lang->{'language'});
         }
     } else {
         if (CONFIG_DEBUG) {
-            log_console('SATURN][RESOURCE LOADER][THEMES', 'Unable to load language file: '.CONFIG_LANGUAGE);
+            log_console('SATURN][RERL][THEMES', 'Unable to load language file: '.CONFIG_LANGUAGE);
         }
         echo alert('ERROR', 'Unable to load language file: '.CONFIG_LANGUAGE);
         log_error('ERROR', 'Unable to load language file: '.CONFIG_LANGUAGE);
