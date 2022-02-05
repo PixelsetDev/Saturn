982d0f96977d268ca90e7be9fd564b38f20e5604
lewmilburn <lewiscmilburn@gmail.com>
2022-02-05 19:04:37 +0000

    Cleaned up code.

--- panel/account/notifications/index.php
+++ panel/account/notifications/index.php
@@ -3,8 +3,7 @@ session_start();
 ob_start();
 include_once __DIR__.'/../../../common/global_private.php';
 if (isset($_GET['dismissNotif'])) {
-    $nid = checkInput('DEFAULT', $_GET['dismissNotif']);
-    update_notification_dismiss($nid);
+    update_notification_dismiss(checkInput('DEFAULT', $_GET['dismissNotif']));
     header('Location: '.CONFIG_INSTALL_URL.'/panel/account/notifications');
 }
 ob_end_flush();
@@ -18,9 +17,7 @@ ob_end_flush();
         <title><?php echo __('Panel:Notifications'); ?> - <?php echo __('General:Saturn'); ?> <?php echo __('Panel:Panel'); ?></title>
     </head>
     <body class="mb-8 dark:bg-neutral-700 dark:text-white">
-        <?php
-            include_once __DIR__.'/../../../common/panel/navigation.php';
-        ?>
+        <?php include_once __DIR__.'/../../../common/panel/navigation.php'; ?>
 
         <header class="bg-white shadow dark:bg-neutral-800">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -35,34 +32,25 @@ ob_end_flush();
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_object()) {
                         foreach ($row as $r) {
-                            ?>
+            ?>
             <div class="flex space-x-4 mb-8">
-                <?php
-                if (strpos(get_notification_title($r), 'not Approved') !== false) {
-                    ?>
+                <?php if (strpos(get_notification_title($r), 'not Approved') !== false) { ?>
                     <div class="bg-red-100 dark:bg-red-600 rounded-full w-20 h-20 text-center">
                         <i class="far fa-thumbs-down fa-2x my-6 text-red-500 dark:text-red-200" aria-hidden="true"></i>
                     </div>
-                    <?php
-                } elseif (strpos(get_notification_title($r), 'Approved') !== false) {
-                    ?>
+                <?php } elseif (strpos(get_notification_title($r), 'Approved') !== false) { ?>
                     <div class="bg-green-100 dark:bg-green-600 rounded-full w-20 h-20 text-center">
                         <i class="far fa-thumbs-up fa-2x my-6 text-green-500 dark:text-green-200" aria-hidden="true"></i>
                     </div>
-                    <?php
-                } elseif (strpos(get_notification_title($r), 'role') !== false) {
-                    ?>
+                <?php } elseif (strpos(get_notification_title($r), 'role') !== false) { ?>
                     <div class="bg-neutral-100 dark:bg-neutral-600 rounded-full w-20 h-20 text-center">
                         <i class="far fa-address-card fa-2x my-6 text-neutral-500 dark:text-neutral-200" aria-hidden="true"></i>
                     </div>
-                    <?php
-                } else {
-                    ?>
+                <?php } else { ?>
                     <div class="bg-neutral-100 dark:bg-neutral-600 rounded-full w-20 h-20 text-center">
                         <i class="far fa-bell fa-2x my-6 text-neutral-500 dark:text-neutral-200" aria-hidden="true"></i>
                     </div>
-                    <?php
-                } ?>
+                <?php } ?>
                 <div>
                     <h2 class="text-xl"><?php echo get_notification_title($r); ?></h2>
                     <p><?php echo get_notification_content($r); ?></p>
@@ -86,32 +74,23 @@ ob_end_flush();
                         foreach ($row2 as $r2) {
                             ?>
                         <div class="flex space-x-4 mb-8">
-                            <?php
-                                if (strpos(get_notification_title($r2), 'not Approved') !== false) {
-                                    ?>
+                            <?php if (strpos(get_notification_title($r2), 'not Approved') !== false) { ?>
                             <div class="bg-red-100 dark:bg-red-600 rounded-full w-20 h-20 text-center">
                                 <i class="far fa-thumbs-down fa-2x my-6 text-red-500 dark:text-red-200" aria-hidden="true"></i>
                             </div>
-                            <?php
-                                } elseif (strpos(get_notification_title($r2), 'Approved') !== false) {
-                                    ?>
+                            <?php } elseif (strpos(get_notification_title($r2), 'Approved') !== false) { ?>
                             <div class="bg-green-100 dark:bg-green-600 rounded-full w-20 h-20 text-center">
                                 <i class="far fa-thumbs-up fa-2x my-6 text-green-500 dark:text-green-200" aria-hidden="true"></i>
                             </div>
-                            <?php
-                                } elseif (strpos(get_notification_title($r2), 'role') !== false) {
-                                    ?>
+                            <?php } elseif (strpos(get_notification_title($r2), 'role') !== false) { ?>
                             <div class="bg-neutral-100 dark:bg-neutral-600 rounded-full w-20 h-20 text-center">
                                 <i class="far fa-address-card fa-2x my-6 text-neutral-500 dark:text-neutral-200" aria-hidden="true"></i>
                             </div>
-                            <?php
-                                } else {
-                                    ?>
+                            <?php } else { ?>
                             <div class="bg-neutral-100 dark:bg-neutral-600 rounded-full w-20 h-20 text-center">
                                 <i class="far fa-bell fa-2x my-6 text-neutral-500 dark:text-neutral-200" aria-hidden="true"></i>
                             </div>
-                            <?php
-                                } ?>
+                            <?php } ?>
                             <div>
                                 <h2 class="text-xl"><?php echo get_notification_title($r2); ?></h2>
                                 <p><?php echo get_notification_content($r2); ?></p>
