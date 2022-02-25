c24b77e0acfddb366657ac7a19f490f3776eeab2
lewmilburn <lewiscmilburn@gmail.com>
2022-02-25 15:11:57 +0000

    Updated translations.

--- panel/admin/security/index.php
+++ panel/admin/security/index.php
@@ -8,8 +8,8 @@
 
     if (isset($_POST['reset-ccv'])) {
         if (ccv_reset()) {
-            log_file('SATURN][SECURITY', 'WARNING: '.get_user_fullname($_SESSION['id']).' reset the Core Checksum Validation values.');
-            header('Location: '.htmlspecialchars($_SERVER['PHP_SELF']).'/?successResetCCV=Core Checksum Validation was reset successfully.');
+            log_file('SATURN][SECURITY', __('General:Warning').': '.get_user_fullname($_SESSION['id']).' '.__('Admin:ResetCCV_Message'));
+            header('Location: '.htmlspecialchars($_SERVER['PHP_SELF']).'/?successResetCCV='.__('Admin:ResetCCV_Success'));
         } else {
             header('Location: '.htmlspecialchars($_SERVER['PHP_SELF']).'/?errorMsg=Unable to reset Core Checksum Validation, an error occurred.');
         }
@@ -38,7 +38,7 @@ ob_end_flush();
         <?php require __DIR__.'/../../../common/admin/navigation.php'; ?>
 
         <div class="px-8 py-4 w-full">
-            <h1 class="text-gray-900 text-3xl">Security Management</h1>
+            <h1 class="text-gray-900 text-3xl"><?php echo __('Admin:SecurityManagement'); ?></h1>
             <?php
             if (isset($errorMsg)) {
                 echo alert('ERROR', $errorMsg);
@@ -51,25 +51,21 @@ ob_end_flush();
             }
             ?>
             <br>
-            <h2 class="text-gray-900 text-2xl mt-8">Core Checksum Validation</h2>
+            <h2 class="text-gray-900 text-2xl mt-8"><?php echo __('Admin:CCV'); ?></h2>
             <?php
             $issue = 0;
             if (!ccv_validate('CONFIG') && !isset($_GET['successResetCCV'])) {
-                echo '<br><div class="duration-300 transform bg-yellow-100 border-l-4 border-yellow-500 hover:-translate-y-2">
-                <div class="p-5 border border-l-0 rounded-r shadow-sm">
-                    <h6 class="mb-2 font-semibold leading-5">[WARNING] Website configuration does not match checksum. <a href="https://docs.saturncms.net/v/'.SATURN_VERSION.'/user-documentation/errors-and-warnings#website-configuration-checksum" class="underline text-xs text-black" target="_blank">Get help.</a></h6>
-                </div>
-            </div><br>';
+                echo alert('WARNING', __('Error:CCV').' <a href="https://docs.saturncms.net/v/'.SATURN_VERSION.'/user-documentation/errors-and-warnings#website-configuration-checksum" class="underline text-xs text-black" target="_blank" rel="noopener">'.__('Error:GetHelp').'</a>', true);
                 $issue = $issue + 1;
             }
             echo $issue.' issues found.';
             ?>
             <div x-data="{open:false}">
-                <a @click="open = true" class="flex-grow cursor-pointer flex w-1/3 text-base font-medium text-red-700">Reset CCV</a>
-                <?php echo display_modal('ERROR', 'Reset Core Checksum Validation', 'Are you sure you want to reset core checksum validation?<br>This action cannot be undone.<br><br>This action will be recorded in the Security Log.', '<form action="'.htmlspecialchars($_SERVER['PHP_SELF']).'" method="post"><div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse flex">
-                                        <input type="submit" id="reset-ccv" name="reset-ccv" value="Reset Core Checksum Validation" class="transition-all duration-200 hover:shadow-lg cursor-pointer w-full flex items-center justify-center px-8 py-1 border border-transparent text-base font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-200 md:py-1 md:text-rg md:px-10">
+                <a @click="open = true" class="flex-grow cursor-pointer flex w-1/3 text-base font-medium text-red-700"><?php echo __('Admin:ResetCCV'); ?></a>
+                <?php echo display_modal('ERROR', __('Admin:ResetCCV'), __('Admin:ResetCCV_Confirm').'<br>'.__('General:CannotBeUndone').'<br><br>'.__('Admin:RecordInSecurityLog'), '<form action="'.htmlspecialchars($_SERVER['PHP_SELF']).'" method="post"><div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse flex">
+                                        <input type="submit" id="reset-ccv" name="reset-ccv" value="'.__('Admin:ResetCCV').'" class="transition-all duration-200 hover:shadow-lg cursor-pointer w-full flex items-center justify-center px-8 py-1 border border-transparent text-base font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-200 md:py-1 md:text-rg md:px-10">
                                         &nbsp;&nbsp;&nbsp;&nbsp;
-                                        <a @click="open=false" class="flex-grow transition-all duration-200 hover:shadow-lg cursor-pointer w-full flex items-center justify-center px-8 py-1 border border-transparent text-base font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-200 md:py-1 md:text-rg md:px-10">Cancel</a>
+                                        <a @click="open=false" class="flex-grow transition-all duration-200 hover:shadow-lg cursor-pointer w-full flex items-center justify-center px-8 py-1 border border-transparent text-base font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-200 md:py-1 md:text-rg md:px-10">'.__('General:Cancel').'</a>
                                     </div>
                                 </form>'); ?>
             </div>
