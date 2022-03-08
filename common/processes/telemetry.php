232cca92a992518651e2d914b4c887aa13166eba
lewmilburn <lewiscmilburn@gmail.com>
2022-03-08 16:24:55 +0000

    Telemetry only sends if site is activated.

--- common/processes/telemetry.php
+++ common/processes/telemetry.php
@@ -5,10 +5,15 @@ function send_data(): string
         $data = array(
             'key' => urlencode(CONFIG_ACTIVATION_KEY),
             'saturn_version' => urlencode(SATURN_VERSION),
-            'php_version' => urlencode(PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION . '.' . PHP_RELEASE_VERSION)
+            'php_version' => urlencode(PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION . '.' . PHP_RELEASE_VERSION),
+            'server_os' => urlencode(PHP_OS_FAMILY)
         );
-        $telemetry = 'https://link.saturncms.net/telemetry/?key=' . $data['key'] . '&saturn_version=' . $data['saturn_version'] . '&php_version=' . $data['php_version'];
-        return file_get_contents($telemetry);
+        if (activation_validate()) {
+            $telemetry = 'https://link.saturncms.net/telemetry/?key=' . $data['key'] . '&saturn_version=' . $data['saturn_version'] . '&php_version=' . $data['php_version'] . '&server_os=' . $data['server_os'];
+            return file_get_contents($telemetry);
+        } else {
+            return '0';
+        }
     } else {
         return '0';
     }
