0f71dacee2e2eb3513d07f11cf67c5339345207b
lewmilburn <lewiscmilburn@gmail.com>
2022-03-11 00:27:25 +0000

    Removed unused code.

--- common/processes/themes.php
+++ common/processes/themes.php
@@ -1,11 +1,5 @@
 <?php
 
-    function list_page_templates(): string
-    {
-        // This will be changed soon but it's staying here because it can.
-        return 'DEFAULT';
-    }
-
     function get_remote_marketplace_version($slug, $type): string
     {
         return file_get_contents('https://link.saturncms.net/marketplace/?version_check='.$type.'&slug='.$slug);
