f6a73d2d9282b65fab5a111a89f43297b8bb8eb1
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-07-27 22:50:39 +0100

    Updated maxlength values to match database.

--- Views/Panel/Account/Login.php
+++ Views/Panel/Account/Login.php
@@ -16,8 +16,8 @@ $TL = new Translate();
             <form class="lg:px-8 lg:py-10 md:px-6 md:py-8 sm:px-4 sm:py-6 px-2 py-4 text-white relative w-full h-full" action="/panel" method="POST">
                 <img src="/Assets/Images/SaturnIcon.png" class="w-24 h-24 bg-white shadow-xl rounded-xl p-1 mx-auto" alt="Saturn">
                 <h1 class="text-3xl text-center font-bold my-8"><?= $TL->TL('SignIn_Message'); ?></h1>
-                <input type="text" name="username" id="username" maxlength="63" required placeholder="<?= $TL->TL('UsernameEmail'); ?>" class="block w-full px-2 py-1 rounded-t-md bg-neutral-100 text-black appearance-none border">
-                <input type="password" name="password" id="password" maxlength="63" required placeholder="<?= $TL->TL('Password'); ?>" class="block w-full px-2 py-1 rounded-b-md bg-neutral-100 text-black appearance-none mb-8 border">
+                <input type="text" name="username" id="username" maxlength="127" required placeholder="<?= $TL->TL('UsernameEmail'); ?>" class="block w-full px-2 py-1 rounded-t-md bg-neutral-100 text-black appearance-none border">
+                <input type="password" name="password" id="password" maxlength="255" required placeholder="<?= $TL->TL('Password'); ?>" class="block w-full px-2 py-1 rounded-b-md bg-neutral-100 text-black appearance-none mb-8 border">
                 <button type="submit" name="login" id="login" class="block w-full px-3 py-2 rounded-md bg-white text-black appearance-none mb-8 shadow-lg hover:shadow-xl bg-neutral-100 hover:bg-white transition duration-200">
                     <i class="fas fa-lock" aria-hidden="true"></i> <?= $TL->TL('SignIn'); ?>
                 </button>
