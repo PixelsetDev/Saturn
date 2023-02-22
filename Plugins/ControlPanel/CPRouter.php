109acc397761686463d67e58bde5244735d3091c
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-02-22 04:21:18 +0000

    Fixed routing.

--- Plugins/ControlPanel/CPRouter.php
+++ Plugins/ControlPanel/CPRouter.php
@@ -8,9 +8,7 @@ class CPRouter
 {
     public function Register(Router $Router): void
     {
-        if (str_contains($_SERVER['REQUEST_URI'], '/panel')) {
-            $Router->GET('/panel', '../Plugins/ControlPanel/Views/Panel.php');
-            $Router->GET('/account/login', '../Plugins/ControlPanel/Views/Login.php');
-        }
+        $Router->GET('/panel', '../Plugins/ControlPanel/Views/Panel.php');
+        $Router->GET('/account/login', '../Plugins/ControlPanel/Views/Login.php');
     }
 }
