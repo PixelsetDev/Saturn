53d41cdf7699ddb9faa5a768930854d992d86f32
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-03-10 18:03:15 +0000

    Updated translation system to allow for external translation files

--- Processes/Saturn/LanguageManager/Translation.php
+++ Processes/Saturn/LanguageManager/Translation.php
@@ -6,19 +6,27 @@ use Saturn\ErrorHandler;
 
 class Translation
 {
+
+    private string $LanguageFile;
+
+    public function __construct($LanguageFile = null)
+    {
+        if ($LanguageFile != null) {
+            $this->LanguageFile = $LanguageFile;
+        } else {
+            $this->LanguageFile = __DIR__.'/../../../Assets/Languages/'.SATURN_LANGUAGE.'.json';
+        }
+    }
+
     public function Translate(string $key): string|bool
     {
-        if (file_exists(__DIR__.'/../../../Assets/Languages/'.SATURN_LANGUAGE.'.json')) {
-            $LanguageFile = file_get_contents(__DIR__.'/../../../Assets/Languages/'.SATURN_LANGUAGE.'.json');
-
-            return $this->DoTranslation($LanguageFile, $key);
-        } elseif (file_exists(__DIR__.'/../../../Assets/Languages/en-gb.json')) {
-            $LanguageFile = file_get_contents(__DIR__.'/../../../Assets/Languages/en-gb.json');
+        if (file_exists($this->LanguageFile)) {
+            $LanguageFile = file_get_contents($this->LanguageFile);
 
             return $this->DoTranslation($LanguageFile, $key);
         } else {
             $ErrorHandler = new ErrorHandler();
-            $ErrorHandler->Fatal('1', 'Language file not found at /../../Assets/Languages/'.SATURN_LANGUAGE.'.json', 'Translation.php', '21');
+            $ErrorHandler->Fatal('1', 'Language file not found at '.$this->LanguageFile, 'Translation.php', '23');
         }
         return false;
     }
