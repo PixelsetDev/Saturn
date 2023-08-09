55e4471cbf73e428eaadfd7c716d80945e063ee2
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-08-09 22:11:37 +0100

    Removed accidental dev tools push

--- Plugins/ControlPanel/Main.php
+++ Plugins/ControlPanel/Main.php
@@ -4,7 +4,6 @@ use ControlPanel\CPRouter;
 use Saturn\HTTP\Router;
 use Saturn\LanguageManager\Translation;
 
-var_dump($_SESSION);
 require_once __DIR__.'/CPRouter.php';
 
 function __CP(string $key): string
