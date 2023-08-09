dee4569f7cc0f218d591259a9ff2a1ff69d01502
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-08-09 22:12:20 +0100

    Updated UUID System

--- Processes/Saturn/AccountManager/UUID.php
+++ Processes/Saturn/AccountManager/UUID.php
@@ -12,22 +12,39 @@ use Saturn\ErrorHandler;
 
 class UUID
 {
-    function Generate($Length = 13)
+    function Generate(): string|null
     {
-        if (function_exists("random_bytes")) {
-            $RandomBytes = random_bytes(ceil($Length / 2));
-        } elseif (function_exists("openssl_random_pseudo_bytes")) {
-            $RandomBytes = openssl_random_pseudo_bytes(ceil($Length / 2));
+        if (function_exists("mt_rand")) {
+            return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                // 32 bits for "time_low"
+                mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+                // 16 bits for "time_mid"
+                mt_rand( 0, 0xffff ),
+
+                // 16 bits for "time_hi_and_version",
+                // four most significant bits holds version number 4
+                mt_rand( 0, 0x0fff ) | 0x4000,
+
+                // 16 bits, 8 bits for "clk_seq_hi_res",
+                // 8 bits for "clk_seq_low",
+                // two most significant bits holds zero and one for variant DCE1.1
+                mt_rand( 0, 0x3fff ) | 0x8000,
+
+                // 48 bits for "node"
+                mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+            );
         } else {
             $EH = new ErrorHandler();
             $EH->SaturnError(
                 '500',
                 'SAT-4',
                 'Function not found.',
-                'No cryptographically secure algorithms are available. One or more of following algorithms are required but are not enabled in your PHP installation: random_bytes OR openssl_random_pseudo_bytes.',
+                'No cryptographically secure algorithms are available. One or more of following algorithms are required but are not enabled in your PHP installation: mt_rand.',
                 SATSYS_DOCS_URL.'/troubleshooting/errors/saturn#sat-4'
             );
         }
-        return substr(bin2hex($RandomBytes), 0, $Length);
+
+        return null;
     }
 }
