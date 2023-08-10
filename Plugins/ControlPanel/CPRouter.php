a6f57fd6a945ddfb9d20e705e4289f8eaab7d675
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-08-10 15:47:49 +0100

    Reducing cognitive complexity of functions.

--- Plugins/ControlPanel/CPRouter.php
+++ Plugins/ControlPanel/CPRouter.php
@@ -9,65 +9,102 @@ use Saturn\SessionManager\Authenticate;
 
 class CPRouter
 {
+    private Router $Router;
+    private Permissions $Permissions;
     public function Register(Router $Router): void
     {
+        $this->Router = $Router;
+        $this->Permissions = new Permissions($_SESSION['UUID']);
+
         require_once __DIR__.'/../../Processes/Saturn/SessionManager/Authenticate.php';
         $Authenticate = new Authenticate();
 
         if (isset($_SESSION['Username']) && isset($_SESSION['Token']) && $Authenticate->Validate()) {
-            $Permissions = new Permissions($_SESSION['UUID']);
+            $this->Account(true);
+            $this->Panel(true);
+        } else {
+            $this->Account(false);
+            $this->Panel(false);
+        }
+    }
 
-            // Logged in
-            $Router->GET(CPURL_Account, '../Plugins/ControlPanel/Views/Account.php');
-            $Router->GET(CPURL_Account.CPURL_Join, '../Plugins/ControlPanel/Views/Account.php');
-            $Router->GET(CPURL_Account.CPURL_Logout, '../Processes/Saturn/AccountManager/Logout.php');
+    private function Account(bool $Authenticated): void
+    {
+        if ($Authenticated) {
+            $this->Router->GET(CPURL_Account, '../Plugins/ControlPanel/Views/Account.php');
+            $this->Router->GET(CPURL_Account.CPURL_Join, '../Plugins/ControlPanel/Views/Account.php');
+            $this->Router->GET(CPURL_Account.CPURL_Logout, '../Processes/Saturn/AccountManager/Logout.php');
+        } else {
+            $this->Router->GET('/account', CPPage_Login);
+            $this->Router->GET('/account/join', '../Plugins/ControlPanel/Views/Join.php');
+            $this->Router->GET('/account/logout', '../Processes/Saturn/AccountManager/Logout.php');
+        }
+    }
 
-            if ($Permissions->HasPermission(['administrator', 'panel_access'], 'OR')) {
-                if ($Permissions->HasPermission(['administrator', 'panel_pages_edit'], 'OR')) {
-                    // Has access to editing pages
-                    $Router->GET(CPURL_Panel.CPURL_Edit, '../Plugins/ControlPanel/Views/Edit.php');
+    private function Panel(bool $Authenticated): void
+    {
+        if ($Authenticated) {
+            if ($this->Permissions->HasPermission(['administrator', 'panel_access'], 'OR')) {
+                if ($this->Permissions->HasPermission(['administrator', 'panel_pages_edit'], 'OR')) {
+                    $this->Pages(true);
                 } else {
-                    // Does not have access to editing pages
-                    $Router->GET(CPURL_Panel.CPURL_Edit, CPPage_NoAccess);
+                    $this->Pages(false);
                 }
 
-                if ($Permissions->HasPermission(['administrator', 'panel_settings'], 'OR')) {
-                    // Has access to editing pages
-                    $Router->GET(CPURL_Panel.CPURL_Plugins, '../Plugins/ControlPanel/Views/Plugins.php');
-                    $Router->GET(CPURL_Panel.CPURL_Users, '../Plugins/ControlPanel/Views/Users.php');
-                    global $SaturnPlugins;
-                    foreach ($SaturnPlugins as $Plugin => $Loaded) {
-                        $Router->GET(CPURL_Panel.CPURL_Plugins.'/'.$Plugin, '../Plugins/ControlPanel/Views/Plugin.php');
-                    }
-                    $DBMS = new DBMS();
-                    $Users = $DBMS->Select('*', 'user', '1', 'all:assoc');
-                    foreach ($Users as $User) {
-                        $Router->GET(CPURL_Panel.CPURL_Users.'/'.$User['uuid'], '../Plugins/ControlPanel/Views/User.php');
-                    }
+                if ($this->Permissions->HasPermission(['administrator', 'panel_settings'], 'OR')) {
+                    $this->Settings(true);
                 } else {
-                    // Does not have access to editing pages
-                    $Router->GET(CPURL_Panel.CPURL_Plugins, CPPage_NoAccess);
-                    $Router->GET(CPURL_Panel.CPURL_Users, CPPage_NoAccess);
+                    $this->Settings(false);
                 }
 
-                // Has access to control panel
-                $Router->GET('/panel', '../Plugins/ControlPanel/Views/Panel.php');
-                $Router->GET('/panel/alert', '../Plugins/ControlPanel/Views/Alert.php');
-                $Router->GET('/panel/api/statistics', '../Plugins/ControlPanel/API/Statistics.php');
+                $this->PanelAccess(true);
             } else {
-                // Does not have access to control panel
-                $Router->GET('/panel/edit', CPPage_NoAccess);
-                $Router->GET('/panel', CPPage_NoAccess);
+                $this->PanelAccess(false);
             }
         } else {
-            // Not logged in
-            $Router->GET('/account', CPPage_Login);
-            $Router->GET('/account/join', '../Plugins/ControlPanel/Views/Join.php');
-            $Router->GET('/account/logout', '../Processes/Saturn/AccountManager/Logout.php');
+            $this->Router->GET('/panel/alert', CPPage_Login);
+            $this->Router->GET('/panel/edit', CPPage_Login);
+            $this->Router->GET('/panel', CPPage_Login);
+        }
+    }
 
-            $Router->GET('/panel/alert', CPPage_Login);
-            $Router->GET('/panel/edit', CPPage_Login);
-            $Router->GET('/panel', CPPage_Login);
+    private function Settings(bool $HasPermission) {
+        if ($HasPermission) {
+            $this->Router->GET(CPURL_Panel.CPURL_Plugins, '../Plugins/ControlPanel/Views/Plugins.php');
+            $this->Router->GET(CPURL_Panel.CPURL_Users, '../Plugins/ControlPanel/Views/Users.php');
+            global $SaturnPlugins;
+            foreach ($SaturnPlugins as $Plugin => $Loaded) {
+                $this->Router->GET(CPURL_Panel.CPURL_Plugins.'/'.$Plugin, '../Plugins/ControlPanel/Views/Plugin.php');
+            }
+            $DBMS = new DBMS();
+            $Users = $DBMS->Select('*', 'user', '1', 'all:assoc');
+            foreach ($Users as $User) {
+                $this->Router->GET(CPURL_Panel.CPURL_Users.'/'.$User['uuid'], '../Plugins/ControlPanel/Views/User.php');
+            }
+        } else {
+            $this->Router->GET(CPURL_Panel.CPURL_Plugins, CPPage_NoAccess);
+            $this->Router->GET(CPURL_Panel.CPURL_Users, CPPage_NoAccess);
+        }
+    }
+
+    private function Pages(bool $HasPermission) {
+        if ($HasPermission) {
+            $this->Router->GET(CPURL_Panel.CPURL_Edit, '../Plugins/ControlPanel/Views/Edit.php');
+        } else {
+            $this->Router->GET(CPURL_Panel.CPURL_Edit, CPPage_NoAccess);
+        }
+    }
+
+    private function PanelAccess(bool $HasPermission) {
+        if ($HasPermission) {
+            $this->Router->GET(CPURL_Panel, '../Plugins/ControlPanel/Views/Panel.php');
+            $this->Router->GET(CPURL_Panel.'/alert', '../Plugins/ControlPanel/Views/Alert.php');
+            $this->Router->GET(CPURL_Panel.'/api/statistics', '../Plugins/ControlPanel/API/Statistics.php');
+        } else {
+            $this->Router->GET(CPURL_Panel.'/api/statistics', CPPage_NoAccess);
+            $this->Router->GET(CPURL_Panel.'/alert', CPPage_NoAccess);
+            $this->Router->GET(CPURL_Panel.CPURL_Edit, CPPage_NoAccess);
+            $this->Router->GET(CPURL_Panel, CPPage_NoAccess);
         }
     }
 }
